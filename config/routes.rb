86e7448ea281760ab15ec3dfc3db652df587f3df
Patrick <pendervp@gmail.com>
2022-03-05 16:09:09 -0500

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :visits do
+  resources :visits
   resources :trial
   # Defines the root path route ("/")
   root "visits#index"
